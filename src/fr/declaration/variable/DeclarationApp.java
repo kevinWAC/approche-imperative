16713aaec55b5f6e039f64127482582054dbac24
kevin <kevin.ulreich@epitech.eu>
2021-12-09 15:32:24 +0100

    TP02 Premières application

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = 30000;
+		int k = 12345678;
+		long l = 10000L;
+		float f = 1.5F;
+		double d = 1.4778D;
+		char c = 't';
+		boolean bool = true;
+		String phrase = "Hello world";
+		String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+		System.out.println(randomString);
+
+	}
+
+}
